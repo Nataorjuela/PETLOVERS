c5fa7b1396b3d01d16b920da12a53b695d954caa
Nataorjuela <nataorjuela62@gmail.com>
2022-07-14 12:13:52 -0500

    login validation

--- PetLoversBack/petlover-chat/src/main/java/petlovers/app/petloverchat/controllers/ChatController.java
+++ PetLoversBack/petlover-chat/src/main/java/petlovers/app/petloverchat/controllers/ChatController.java
@@ -8,7 +8,9 @@ import petlovers.app.petloverchat.dtos.MessageDTO;
 @RestController
 @RequestMapping("api")
 @CrossOrigin(origins="*")
+
 public class ChatController {
+
     @PostMapping("/messages")
     public void message(@RequestBody MessageDTO messageDTO){
         Pusher pusher = new Pusher("1432809", "6136aa3612d9d2f6aaa0", "66ffa418febd1ba09d3d");
